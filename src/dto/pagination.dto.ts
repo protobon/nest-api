bb77ad377f8b47dea40a286053ec55af7ed7f092
ayrton <ayrtoncoelhods@gmail.com>
2024-12-01 21:23:37 -0300

    pagination types

--- src/dto/pagination.dto.ts
+++ src/dto/pagination.dto.ts
@@ -1,11 +1,14 @@
+import { Type } from "class-transformer";
 import { IsNumber, IsOptional } from "class-validator";
 
 export class PaginationDto {
     @IsNumber()
     @IsOptional()
+    @Type(() => Number)
     page?: number = 1;
 
     @IsNumber()
     @IsOptional()
+    @Type(() => Number)
     limit?: number = 10;
 }
